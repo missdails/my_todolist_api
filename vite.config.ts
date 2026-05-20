d18c510ad5a8388412dad515c731d5272ea7b599
Leia <806204245@qq.com>
2026-05-20 15:54:33 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'/https://github.com/missdails/my-todolist-pinia-/',
+  base:'/my-todolist-pinia-/',
   resolve:{
     alias: {
       '@': path.resolve(__dirname, './src'),
